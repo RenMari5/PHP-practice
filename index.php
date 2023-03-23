7900eac54d3347e2c57bb3abb52d7355b214ee46
Renee Paczkowski <rpaczkowski@gmail.com>
2023-03-23 15:05:42 -0400

    Added comments about case sensitivity

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
 <?php
 $txt = "PHP";
 echo "I love $txt";
 // This is a comment in PHP
 ?>
+
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>"; //will not work
+echo "My boat is " . $coLOR . "<br>"; //will not work
+?>
+
+</body>
+</html>
